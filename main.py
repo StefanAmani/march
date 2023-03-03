54057a14a789e913c2b3e7da33bf9995e9d29c40
StefanAmani <steveamani12@gmail.com>
2023-03-03 15:46:34 -0600

    debug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import openai
 import os
 from flask import Flask, render_template, request
+import logging
 
 DEBUG=True
 
@@ -13,28 +14,34 @@ openai.api_key = "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"
 # Define home route
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        # Get user input from form
-        user_input = request.form['user_input']
-
-        # Call OpenAI API to generate response
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_input,
-            temperature=0.5,
-            max_tokens=4000,
-            n=1,
-            stop=None,
-            timeout=10,
-        )
-
-        # Extract response text from API response
-        response_text = response.choices[0].text.strip()
-
-        # Return response text to template
-        return render_template('home.html', response_text=response_text)
-    else:
-        return render_template('home.html')
+    try:
+        if request.method == 'POST':
+            # Get user input from form
+            user_input = request.form['user_input']
+            logging.debug("User input line 21")
+            # Call OpenAI API to generate response
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=user_input,
+                temperature=0.5,
+                max_tokens=4000,
+                n=1,
+                stop=None,
+                timeout=10,
+            )
+            logging.debug("Response line 32")
+            # Extract response text from API response
+            response_text = response.choices[0].text.strip()
+            logging.debug("response text line 35")
+            # Return response text to template
+            return render_template('home.html', response_text=response_text)
+        else:
+            logging.debug("User input line 39")
+            return render_template('home.html')
+            
+    except: 
+            logging.error("Failed to Run")
+        
 
 #if __name__ == '__main__':
     #port = int(os.environ.get("PORT", 80))
